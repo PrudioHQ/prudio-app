76174f41adf501079e1426f4f2eba133534825eb
Helder Duarte <cossou@gmail.com>
2014-11-19 17:02:12 +0000

    New home welcome

--- app/views/_layout/blank.blade.php
+++ app/views/_layout/blank.blade.php
@@ -1,13 +1,23 @@
-<!doctype html>
+<!DOCTYPE html>
 <html>
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title>{{ isset($title) ? $title : 'Prud.io' }}</title>
+        <meta name="description" content="">
+        <meta name="keywords" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>{{ isset($title) ? $title : 'Prud.io' }}</title>
-</head>
+        <link href="favicon.ico" rel="shortcut icon" type="image/vnd.microsoft.icon" />
+        <link rel="stylesheet" href="/css/welcome.css">
+        <link href="http://fonts.googleapis.com/css?family=Quicksand:300,400,700" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+        
+        @yield('content')
 
-<body>
-	@yield('content')
-</body>
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+        @yield('prudio-js')
+
+    </body>
 </html>
--- app/views/welcome.blade.php
+++ app/views/welcome.blade.php
@@ -2,7 +2,42 @@
 
 @section('content')
 
-<h1>Welcome to Prud.io</h1>
-<p>Soon</p>
+	<div class="logo">
+		<img src="/images/logo-beta.png" alt="Prudio">
+	</div>
+	
+	<div class="content">
+		<h1>Welcome to Prudio</h1>
+		<p>
+			We are still building Prudio. 
+			<br>
+			Want to <a href="https://assembly.com/prudio-live-chat-for-your-slack-team" target="_blank">join the team</a>?
+			<br>
+			<br>
+			<a href="mailto:hello@prud.io">hello@prud.io</a>
+		</p>
+	</div>
+
+	<div class="this-is-prudio">
+		<img src="/images/this-is-prudio.png" alt="Check it!">
+	</div>
+	
+@stop
+
+@section('prudio-js')
+	
+	<!-- This is where PRUDIO starts --> 
+	<script src="http://chat.prud.io/client.js?token=77475a1d-d347-4514-b0b3-1f01c1a205ea" async></script>
+    <script>
+
+    	// This is optional 
+    	// and there are more options available!
+
+        window._PrudioSettings = {
+        	title: 'Prudio'
+        };
+
+    </script>
+    <!-- Yep... just this! THE END! -->
 
 @stop
