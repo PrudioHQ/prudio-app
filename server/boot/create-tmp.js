011f737971d7f26530e69b9af8061470375fa676
Helder Duarte <cossou@gmail.com>
2014-12-30 11:54:16 +0000

    Create TMP error

--- server/boot/create-tmp.js
+++ server/boot/create-tmp.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 var environment = process.env.NODE_ENV || 'development';
 
 fs.stat('../tmp', function(err, stats) {
-  if (!stats.isDirectory()) {
+  if (stats && !stats.isDirectory()) {
     console.log("Creating tmp directory...");
     fs.mkdirSync("../tmp");
   }
