116f8c9f65a3c089a31771abf86f4c138175fbe3
Helder Duarte <cossou@gmail.com>
2015-01-05 23:40:16 +0000

    Refactor, simplify the installation and execution of the app. Merged package.json and gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,110 @@
-var gulp = require('gulp');
-var chug = require('gulp-chug');
-var rename = require('gulp-rename');
-var loopbackAngular = require('gulp-loopback-sdk-angular');
+var gulp = require('gulp'),
+    usemin = require('gulp-usemin'),
+    wrap = require('gulp-wrap'),
+    connect = require('gulp-connect'),
+    watch = require('gulp-watch'),
+    minifyCss = require('gulp-minify-css'),
+    minifyJs = require('gulp-uglify'),
+    concat = require('gulp-concat'),
+    less = require('gulp-less'),
+    rename = require('gulp-rename'),
+    minifyHTML = require('gulp-minify-html'),
+    rename = require('gulp-rename'),
+	loopbackAngular = require('gulp-loopback-sdk-angular');
+
+var paths = {
+    scripts: 'dashboard/src/js/**/*.*',
+    styles: 'dashboard/src/less/**/*.*',
+    images: 'dashboard/src/img/**/*.*',
+    templates: 'dashboard/src/templates/**/*.html',
+    index: 'dashboard/src/index.html',
+    bower_fonts: 'dashboard/src/components/**/*.{ttf,woff,eof,svg}',
+};
+
+/**
+ * Handle bower components from index
+ */
+gulp.task('usemin', function() {
+    return gulp.src(paths.index)
+        .pipe(usemin({
+            js: [minifyJs(), 'concat'],
+            css: [minifyCss({keepSpecialComments: 0}), 'concat'],
+        }))
+        .pipe(gulp.dest('build/'));
+});
+
+/**
+ * Copy assets
+ */
+gulp.task('build-assets', ['copy-bower_fonts']);
+
+gulp.task('copy-bower_fonts', function() {
+    return gulp.src(paths.bower_fonts)
+        .pipe(rename({
+            dirname: '/fonts'
+        }))
+        .pipe(gulp.dest('build/lib'));
+});
+
+/**
+ * Handle custom files
+ */
+gulp.task('build-custom', ['custom-images', 'custom-js', 'custom-less', 'custom-templates']);
+
+gulp.task('custom-images', function() {
+    return gulp.src(paths.images)
+        .pipe(gulp.dest('build/img'));
+});
+
+gulp.task('custom-js', function() {
+    return gulp.src(paths.scripts)
+        .pipe(minifyJs())
+        .pipe(concat('dashboard.min.js'))
+        .pipe(gulp.dest('build/js'));
+});
+
+gulp.task('custom-less', function() {
+    return gulp.src(paths.styles)
+        .pipe(less())
+        .pipe(gulp.dest('build/css'));
+});
+
+gulp.task('custom-templates', function() {
+    return gulp.src(paths.templates)
+        .pipe(minifyHTML())
+        .pipe(gulp.dest('build/templates'));
+});
+
+/**
+ * Watch custom files
+ */
+gulp.task('watch', function() {
+    gulp.watch([paths.images], ['custom-images']);
+    gulp.watch([paths.styles], ['custom-less']);
+    gulp.watch([paths.scripts], ['custom-js']);
+    gulp.watch([paths.templates], ['custom-templates']);
+    gulp.watch([paths.index], ['usemin']);
+});
+
+gulp.task('livereload', function() {
+    gulp.src(['build/**/*.*'])
+        .pipe(watch())
+        .pipe(connect.reload());
+});
+
+/**
+ * Gulp publish lbServices
+ */
 
 gulp.task('lb-services', function () {
     return gulp.src('./server/server.js')
 	    .pipe(loopbackAngular())
 	    .pipe(rename('lb-services.js'))
-	    .pipe(gulp.dest('./dashboard/dist/js'));
+	    .pipe(gulp.dest('build/js'));
 });
 
-gulp.task('run-external', function () {
-    return gulp.src('./dashboard/gulpfile.js')
-        .pipe(chug());
-});
-
-gulp.task('default', ['lb-services', 'run-external']);
+/**
+ * Gulp tasks
+ */
+gulp.task('build', ['usemin', 'lb-services', 'build-assets', 'build-custom']);
+gulp.task('default', ['lb-services', 'build', 'livereload', 'watch']);
