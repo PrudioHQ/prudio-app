cee0870f2d7cac9a439ad063e96d16c060d3d2b9
Helder Duarte <cossou@gmail.com>
2015-01-07 15:24:30 +0000

    Login controller handle

--- dashboard/src/js/controllers/auth-ctrl.js
+++ dashboard/src/js/controllers/auth-ctrl.js
@@ -8,6 +8,6 @@ angular
 
 function AuthCtrl($scope) {
 
-  console.log("AlertsCtrl");
+  console.log("AuthCtrl");
 
 }
--- dashboard/src/js/controllers/auth/login-ctrl.js
+++ dashboard/src/js/controllers/auth/login-ctrl.js
@@ -1,12 +1,28 @@
 angular
 .module('RDash')
-.controller('loginCtrl', ['$scope', 'User', function($scope, User) {
+.controller('loginCtrl', ['$scope', '$location', 'User', function($scope, $location, User) {
 
-  $scope.form = {};
+	$scope.credentials = {};
 
-  $scope.login = function() {
-    var cred = $scope.form;
-    User.login(cred);
-  };
+ 
+	$scope.login = function() {
+		$scope.loginResult = User.login({
+			include: 'user',
+			rememberMe: $scope.credentials.remember
+		}, $scope.credentials,
+		function() {
+			var next = $location.nextAfterLogin || '/';
+			$location.nextAfterLogin = null;
+		  
+			if(next === '/login') {
+		    	next = '/';
+			}
+
+		  	$location.path(next);
+		},
+		function(res) {
+		  	$scope.loginError = res.data.error;
+		});
+	};
 
 }]);
