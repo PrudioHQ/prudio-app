82b82527d5cb4d9c30997beefaba91525213c2e0
Marios Antonoudiou <marios.antonoudiou@gmail.com>
2015-01-08 23:55:03 +0200

    Add registration logic

--- dashboard/src/js/controllers/auth/register-ctrl.js
+++ dashboard/src/js/controllers/auth/register-ctrl.js
@@ -1,13 +1,23 @@
 angular
 .module('RDash')
-.controller('registerCtrl', ['$scope', 'User', function($scope, User) {
+.controller('registerCtrl', ['$scope', 'User', '$state', function($scope, User, $state) {
 
-  $scope.form = {};
-
-  $scope.register = function() {
-    var cred = $scope.form;
-    User.create(cred).then(function(){
-      console.log("User created");
+  $scope.register = function(form) {
+    if (form.$invalid) {
+      // return
+    }
+    var newUser = {
+      fname: form.fname,
+      lname: form.lname,
+      email: form.email,
+      password: form.password
+    };
+    User.create(newUser, function(){
+      // TODO: Notify the user that has been successfully created
+      $state.go('master');
+    }, function(res){
+      console.log(res);
+      $scope.errorMessage = res.data.error.message
     });
   };
 
