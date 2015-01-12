6a6cbbaac930bfe37211ff5bc502dec47f0b1a9a
Marios Antonoudiou <marios.antonoudiou@gmail.com>
2015-01-12 21:35:15 +0200

    Broken auth

--- server/server.js
+++ server/server.js
@@ -3,6 +3,9 @@ var boot = require('loopback-boot');
 
 var app = module.exports = loopback();
 
+var PassportConfigurator = require('loopback-component-passport').PassportConfigurator;
+var passportConfigurator = new PassportConfigurator(app);
+
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
 boot(app, __dirname);
@@ -13,6 +16,32 @@ boot(app, __dirname);
 // Example:
 var websitePath = require('path').resolve(__dirname, '../build');
 app.use(loopback.static(websitePath));
+
+app.use(loopback.session({ secret: 'keyboard cat' }));
+// Load the provider configurations
+var config = {};
+try {
+  config = require('./providers.json');
+} catch(err) {
+  console.error('Please configure your passport strategy in `providers.json`.');
+  console.error('Copy `providers.json.template` to `providers.json` and replace the clientID/clientSecret values with your own.');
+  process.exit(1);
+}
+// Initialize passport
+passportConfigurator.init();
+
+// Set up related models
+passportConfigurator.setupModels({
+  userModel: app.models.user,
+  userIdentityModel: app.models.userIdentity,
+  userCredentialModel: app.models.userCredential
+});
+// Configure passport strategies for third party auth providers
+for(var s in config) {
+  var c = config[s];
+  c.session = c.session !== false;
+  passportConfigurator.configureProvider(s, c);
+}
 
 // Requests that get this far won't be handled
 // by any middleware. Convert them into a 404 error
