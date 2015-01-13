1e5333fc88f8fbea266d3d489e71914775ad21aa
Helder Duarte <cossou@gmail.com>
2015-01-13 14:55:26 +0000

    Added e-mail example when user registers an account

--- common/models/user.js
+++ common/models/user.js
@@ -1,3 +1,32 @@
 module.exports = function(User) {
 
+	User.afterSave = function(next) {
+
+		User.app.models.Email.send({
+			to: this.email,
+			from: 'hello@prud.io',
+			subject: 'Welcome to Prud.io',
+			template: {
+				name: "signup-e-mail"
+			},
+			merge_vars: [
+				{
+					"rcpt": this.email,
+					"vars": [
+						{
+							name: "name",
+							content: this.fname
+						}]
+				}
+			]
+		}, 
+		function(err, result) {
+			if(err) {
+				console.log('Upppss something crash');
+				console.log(err);
+			}
+
+			return next(err);
+		});
+	}
 };
