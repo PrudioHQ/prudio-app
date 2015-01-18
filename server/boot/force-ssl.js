add42b8aaef1b396233a97e72e203b1a0109b47a
Helder Duarte <cossou@gmail.com>
2015-01-18 11:20:35 +0000

    Correct if statment

--- server/boot/force-ssl.js
+++ server/boot/force-ssl.js
@@ -2,11 +2,12 @@ module.exports = function forceSSL(server) {
 	var environment = process.env.NODE_ENV || 'development';
 
 	if (environment === 'production') {
-		server.get('*', function(req, res, next){
-			if (req.headers['x-forwarded-proto'] !== 'https')
-				res.redirect('https://app.prud.io'+req.url);
-			else
+		server.get('*', function(req, res, next) {
+			if (req.headers['x-forwarded-proto'] !== 'https') {
+				res.redirect('https://app.prud.io' + req.url);				
+			} else {
 				next();
+			}
 		});
 	}
 };
