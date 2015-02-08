03760a474cb2487595eab6cf52e83b4891f27ab9
Marios Antonoudiou <marios.ant@qboss.eu>
2015-02-08 18:26:39 +0200

    Implement basic role for accounts

--- server/boot/teamMember-role.js
+++ server/boot/teamMember-role.js
@@ -1,22 +1,32 @@
 module.exports = function(app) {
   var Role = app.models.Role;
-  
+
    Role.registerResolver('teamMember', function(role, context, cb) {
-     
+
     function reject(msg) {
       console.log(msg);
       process.nextTick(function() {
       cb(null, false);
       });
     }
-    
+
     var userId = context.accessToken.userId;
-      console.log(context.accessToken);
       if (!userId) {
-      return reject("Invalid access token");
+      return reject('Invalid access token');
     }
-    
-    cb(null, true);
-     
+
+    context.model.findById(context.modelId, function provideModel(err, model) {
+      if (err || !model) { return reject('No relative model found.');}
+
+      model.users.findById(userId, function postFind(err, users) {
+        if (err || !users) {
+          reject("Unauthorized!");
+        } else {
+          cb(null, true);
+        }
+
+      });
+    });
+
    });
 };
