36d0dfc72b08a3f0d5c1cf670a628d24494e1606
Helder Duarte <cossou@gmail.com>
2015-02-16 16:24:59 +0000

    Refactor the App model

--- common/models/app.js
+++ common/models/app.js
@@ -3,7 +3,7 @@ var qs      = require('querystring');
 
 module.exports = function(App) {
 
-	// Hide almost all methods 
+	// Hide almost all methods
 	// Only because we need it in the lb-services (Angular SDK)
 
     App.disableRemoteMethod('find', true);
@@ -18,47 +18,49 @@ module.exports = function(App) {
     App.disableRemoteMethod('prototype.updateAttributes', true);
     App.disableRemoteMethod('prototype.__get__account', true);
 
+    App.validatesUniquenessOf('appId');
+
 
     var environment = process.env.NODE_ENV || 'development';
     var baseUrl = 'http://localhost:5000';
-    
+
     if (environment !== 'development') {
         baseUrl = 'http://chat.prud.io';
     }
 
     /*
     * Connect method
-    * 
+    *
     */
     App.connect = function(id, next) {
 
         App.findById(id, function(err, app) {
             if (err) {
-                return next(err); 
+                return next(err);
             } else if (!app) {
                 return next(new Error('App not found!'));
             }
 
             var query = {
-                appid: app.appid,
-                token: app.slack_api_token
+                appid: app.appId,
+                token: app.slackApiToken
             }
 
             request
                 .post({url: baseUrl + '/app/connect?' + qs.stringify(query), json: true}, function (error, response, body) {
                     if (error) {
-                        return next(null, false, error); 
+                        return next(null, false, error);
                     }
-                    return next(null, body.success, body.message);         
+                    return next(null, body.success, body.message);
                 })
                 .on('error', function(err) {
                     console.log(err);
                 });
         });
     }
-     
+
     App.remoteMethod(
-        'connect', 
+        'connect',
         {
             description: "Connect an App",
             accepts: {arg: 'id', type: 'any', required: true, description: 'PersistedModel id'},
@@ -75,37 +77,37 @@ module.exports = function(App) {
 
     /*
     * Disconnect method
-    * 
+    *
     */
     App.disconnect = function(id, next) {
 
         App.findById(id, function(err, app) {
             if (err) {
-                return next(err); 
+                return next(err);
             } else if (!app) {
                 return next(new Error('App not found!'));
             }
 
             var query = {
-                appid: app.appid,
-                token: app.slack_api_token
+                appid: app.appId,
+                token: app.slackApiToken
             }
 
             request
                 .post({url: baseUrl + '/app/disconnect?' + qs.stringify(query), json: true}, function (error, response, body) {
                     if (error) {
-                        return next(null, false, error); 
+                        return next(null, false, error);
                     }
-                    return next(null, body.success, body.message);         
+                    return next(null, body.success, body.message);
                 })
                 .on('error', function(err) {
                     console.log(err);
                 });
         });
     }
-     
+
     App.remoteMethod(
-        'disconnect', 
+        'disconnect',
         {
             description: "Connect an App",
             accepts: {arg: 'id', type: 'any', required: true, description: 'PersistedModel id'},
@@ -124,8 +126,23 @@ module.exports = function(App) {
     }
 
     App.beforeCreate = function(next, app) {
-        app.created = new Date();
+
+        // Creates a 32 char random string
+        function makeid()
+        {
+            var text = "";
+            var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+            for( var i=0; i < 32; i++ )
+                text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+            return text;
+        }
+
+        app.appId    = makeid();
+        app.created  = new Date();
         app.modified = new Date();
+
         next();
     }
 
