ead53733f2d6867f5547e1df64422abdab114eae
Helder Duarte <cossou@gmail.com>
2015-02-18 18:32:54 +0000

    Create App with some more functions

--- dashboard/src/js/controllers/apps/createApp-ctrl.js
+++ dashboard/src/js/controllers/apps/createApp-ctrl.js
@@ -9,37 +9,26 @@ function createAppCtrl($scope, $http, User, Account) {
 
     var url = "https://slack.com/api/users.list";
 
-    $scope.token = [];
-    $scope.bots  = [];
+    $scope.user   = {};
+    $scope.tokens = [];
+    $scope.bots   = [];
 
     User.getCurrent(function(user, req, err) {
-        console.log("User: ", user);
-        Account.findById({ id: user.defaultAccountId }, function(account, req, err) {
-            console.log("Account: ", account);
-            Account.prototype$__get__externalProviderTokens({ id: account.id }, function(tokens, req, err) {
-                console.log("Tokens: ", tokens);
+        $scope.user = user;
 
+        Account.findById({ id: user.defaultAccountId }, function(account, req, err) {
+            Account.prototype$__get__externalProviderTokens({ id: account.id }, function(tokens, req, err) {
                 $scope.tokens = tokens;
-                console.log("Tokens: ", req);
-                console.log("Tokens: ", err);
             });
         });
     });
 
+
     $scope.getBots = function(token) {
-        console.log("Token: " + token);
-        $http.post(url + "?token=xoxp-3045626515-3045626517-3758533827-9009a2", { token: token }).
-            success(function(data, status, headers, config) {
-                // this callback will be called asynchronously
-                // when the response is available
-                $scope.bots = data.members;
-                console.log("Members: ", $scope.bots);
-            }).
-            error(function(data, status, headers, config) {
-                // called asynchronously if an error occurs
-                // or server returns response with an error status.
-                console.log("Error: ", headers)
-            });
+        console.log(token);
+        Account.listSlackMembers({ id: $scope.user.defaultAccountId, fk: token }, function(members, req, err) {
+            console.log("Members: ", members);
+        })
     };
 
 
