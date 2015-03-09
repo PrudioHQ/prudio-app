673855abc48399f11a3329097176709b73c99c2c
Helder Duarte <cossou@gmail.com>
2015-03-09 15:20:46 +0000

    Added ability to delete and turn on/off apps
    Updated bower font awesome to 4.2

--- dashboard/src/js/controllers/apps/viewApp-ctrl.js
+++ dashboard/src/js/controllers/apps/viewApp-ctrl.js
@@ -3,21 +3,39 @@
  */
 
 angular.module('RDash')
-    .controller('viewAppCtrl', ['$state', '$scope', '$stateParams', 'User', 'Account', viewAppCtrl]);
+    .controller('viewAppCtrl', ['$state', '$scope', '$stateParams', '$location', 'User', 'Account', viewAppCtrl]);
 
-function viewAppCtrl($state, $scope, $stateParams, User, Account) {
+function viewAppCtrl($state, $scope, $stateParams, $location, User, Account) {
 
     $scope.application = {};
 
     User.getCurrent(function(user, req, err) {
-
-        console.log("UID: ", user.defaultAccountId);
-        console.log("AID: ", $stateParams.appId);
-
-
         Account.apps.findById({ id: user.defaultAccountId, fk: $stateParams.appId }, function(app, req, err) {
-            console.log(app);
             $scope.application = app;
         });
+
+        $scope.turnOn = function() {
+            Account.apps.updateById({ id: user.defaultAccountId, fk: $stateParams.appId }, { online: true }, function(app, req, err) {
+                console.log("Online");
+                $scope.application = app;
+            });
+        }
+
+        $scope.turnOff = function() {
+            Account.apps.updateById({ id: user.defaultAccountId, fk: $stateParams.appId }, { online: false }, function(app, req, err) {
+                console.log("Offline");
+                $scope.application = app;
+            });
+        }
+
+        $scope.delete = function() {
+            Account.apps.destroyById({ id: user.defaultAccountId, fk: $stateParams.appId }, function(req, err) {
+                console.log("Delete");
+                $scope.application = null;
+                $location.path('/apps');
+            });
+        }
     });
+
+    
 }
